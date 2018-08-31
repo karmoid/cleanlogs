a02a47833ef89333d2364364825b21f22dd3f5c1
karmoid <karmoc@gmail.com>
2018-08-31 13:38:34 +0200

    First commit V1.01 2006

---
+++ BaseObjects.pas
@@ -0,0 +1,480 @@
+{-----------------------------------------------------------------------------
+ Unit Name: BaseObjects
+ Author:    Marc
+ Date:      01-oct.-2006
+ Purpose:   Définition des classes de base manipulées par le programme de
+            nettoyage des fichiers.
+ History:
+            Version 0.9Alpha bour Brink's France fait le week-end chez moi.
+            Pour supprimer tous les fichiers oubliés dans les répertoires
+            BAMBOO
+-----------------------------------------------------------------------------}
+unit BaseObjects;
+
+interface
+uses
+  Classes,
+  Contnrs,
+  Windows,
+  Sysutils;
+
+var
+  FileSeq : Cardinal = 0;
+
+Type
+  tKindAccess = (kaCreated, kaAccessed, kaModified);
+  tKindPeriod = (kpDay, kpWeek, kpMonth, kpYear);
+  tTaillePLancher = (Kilo=1024, Mega=1024*1024, Tera=1024*1024*1024);
+  tOptions = (opRecursif,opExecute,opForce);
+
+const
+  AccessLib : array[tKindAccess] of string = ('création', 'dernier accès', 'modification');
+  PeriodLib : array[tKindPeriod] of string  = ('jour', 'semaine', 'mois', 'année');
+  SetOptionsLib : array[tOptions] of string = ('recursif','execute','force');
+  ClrOptionsLib : array[tOptions] of string = ('plat','simulation','confirmation');
+
+type
+  tOptionset = set of tOptions;
+
+  tCleanException = class(Exception)
+  end;
+
+  tFichier = class
+  private
+    fID : Cardinal;
+    fFileName : TFileName;
+    fDates : array[tKindAccess] of TDateTime;
+    fFileSize : Cardinal;
+    fPath : string;
+    procedure SetFileName(const Value: TFileName);
+    procedure SetID(const Value: Cardinal);
+    class function GetNextID : Integer;
+    procedure SetFileSize(const Value: Cardinal);
+    class function FileTimeToDateTime(FileTime: TFileTime): TDateTime;
+    function GetTailleToString: string;
+    function GetKiloToString: string;
+    function GetOctetToString: string;
+    procedure SetPath(const Value: string);
+    function GetDates(const Index: tKindAccess): TDateTime;
+    procedure SetDates(const Index: tKindAccess; Value : TDateTime);
+  public
+    { Identifiant unique du fichier dans le programme. Séquenceur Run-Time }
+    property ID : Cardinal read FID write SetID;
+    { Nom complet du fichir en UNC ou en format Lettre+Chemin }
+    property FileName : tFileName read FFileName write SetFileName;
+    { Date de création du fichier }
+    property DateCreated : TDateTime Index kaCreated read GetDates write SetDates;
+    { Date de dernier accès au fichier }
+    property DateAccessed : TDateTime Index kaAccessed read GetDates write SetDates;
+    { Date de dernière modification }
+    property DateModified : TDateTime Index kaModified read GetDates write SetDates;
+    property Dates [const Index : tKindAccess] : TDateTime read GetDates write SetDates;
+    { Taille liberée par la suppression du fichier }
+    property FileSize : Cardinal read FFileSize write SetFileSize;
+    { Chemin d'accès au fichier }
+    property Path : string read FPath write SetPath;
+    { Taille représentée sous forme de chaîne }
+    property TailleToString : string read GetTailleToString;
+    { Taille en Kilo Octets représentée sous forme de chaîne }
+    property KiloToString: string read GetKiloToString;
+    { Taille en octets représentée sous forme de chaîne }
+    property OctetToString: string read GetOctetToString;
+    { Construction de l'objet par un record Findfirst/Findnext }
+    constructor Create(const RecInfo : TSearchRec);
+  end;
+
+  tCritere = class
+  private
+    fAccess : tKindAccess;
+    fPeriod : tKindPeriod;
+    fValue  : Integer;
+    fSpec   : TStrings;
+    procedure SetAccess(const Value: tKindAccess);
+    procedure SetPeriod(const Value: tKindPeriod);
+    procedure SetSpec(const Value: TStrings);
+    procedure SetValue(const Value: Integer);
+  public
+    { Type d'accès à tester }
+    property Access : tKindAccess read FAccess write SetAccess;
+    { Type de période à tester }
+    property Period : tKindPeriod read FPeriod write SetPeriod;
+    { Nombre de période à tester }
+    property Value  : Integer read FValue write SetValue;
+    { Spécification de fichier à prendre en compte }
+    property Spec   : TStrings read FSpec write SetSpec;
+    { Constructeur du critère de sélection }
+    constructor Create(StValue: string);
+    { Destructeur de l'objet }
+    destructor Destroy; override;
+    { Indique si le fichier doit être pris en compte }
+    function AcceptFile(Fic : tFichier) : Boolean;
+    { Donne une représentation correcte de l'objet }
+    function ToString : string;
+  end;
+
+  tTask = class(TObjectList)
+  private
+    fListCriteres : TObjectList;
+    fPath         : string;
+    fOptionsSet   : tOptionSet;
+    bOptionsFOund : tOptionSet;
+    fTraceFile    : TFileName;
+    bTraceFile    : Boolean;
+    function GetOptionsSet(const Index: tOptions): Boolean;
+    procedure SetOptionsSet(const Index: tOptions; const Value: Boolean);
+    procedure ExploiteOneCritere(var Crit : string);
+    function GetOptionToString: string;
+    function getOptionSet: tOptionset;
+    procedure SetOptionSet(const Value: tOptionset);
+    procedure SetTraceFile(const value: string);
+
+  public
+    property ListCriteres : TObjectList read fListCriteres;
+    property Path         : string read fPath write fpath;
+    property OptionSet : tOptionset read getOptionSet write SetOptionSet;
+    property OptionsSet [const Index: tOptions] : Boolean read getOptionsSet write SetOptionsSet;
+    property TraceFile    : TFileName read fTraceFile write SetTraceFile;
+    property OptionToString : string read GetOptionToString;
+    constructor Create; overload;
+    constructor Create(Value : string); overload;
+    destructor Destroy; override;
+    procedure ExploiteCritere(Crit : string);
+    procedure ExploitOption(Opt : string);
+    function GetValueTraceFile(const value : string) : string;
+    function GetValueOptionSet(const value : tOptionSet) : tOptionSet;
+  end;
+
+implementation
+
+uses DateUtils;
+
+{ tFichier }
+
+constructor tFichier.Create(const RecInfo: TSearchRec);
+begin
+  inherited Create;
+  ID := GetNextID;
+  FileName := RecInfo.Name;
+  DateAccessed := FileTimeToDateTime(RecInfo.FindData.ftLastAccessTime);
+  DateCreated := FileTimeToDateTime(RecInfo.FindData.ftCreationTime);
+  DateModified := FileTimeToDateTime(RecInfo.FindData.ftLastWriteTime);
+  FileSize := (RecInfo.FindData.nFileSizeHigh SHL 32) or RecInfo.FindData.nFileSizeLow;
+end;
+
+class function tFichier.GetNextID: Integer;
+begin
+  Inc(FileSeq);
+  Result := FileSeq;
+end;
+
+procedure tFichier.SetFileName(const Value: TFileName);
+begin
+  FFileName := Value;
+end;
+
+procedure tFichier.SetFileSize(const Value: Cardinal);
+begin
+  FFileSize := Value;
+end;
+
+procedure tFichier.SetID(const Value: Cardinal);
+begin
+  FID := Value;
+end;
+
+class function tfichier.FileTimeToDateTime(FileTime: TFileTime): TDateTime;
+{=================================================================}
+{ fonction permettant de convertir des date de type FileTime      }
+{ en Date de type DateTime                                        }
+{=================================================================}
+var
+  LocalFileTime: TFileTime;
+  SystemTime: TSystemTime;
+begin
+  FileTimeToLocalFileTime(FileTime, LocalFileTime);
+  FileTimeToSystemTime(LocalFileTime, SystemTime);
+  Result := SystemTimeToDateTime(SystemTime);
+end;
+
+
+
+function tFichier.GetTailleToString: string;
+begin
+  if FileSize>Ord(Tera) then
+    Result := IntToStr(FileSize div Ord(Tera))+','+
+              IntToStr((FileSize mod Ord(Tera)) div Ord(Mega))+'TiB'
+  else if FileSize>Ord(Mega) then
+    Result := IntToStr(FileSize div Ord(Mega))+','+
+              IntToStr((FileSize mod Ord(Mega)) div Ord(Kilo))+'MiB'
+  else if FileSize>Ord(Kilo) then
+    Result := IntToStr(FileSize div Ord(Kilo))+','+
+              IntToStr((FileSize mod Ord(Kilo)))+'KiB'
+  else
+    Result := IntToStr(FileSize)+'B';
+end;
+
+function tFichier.GetKiloToString: string;
+begin
+  if FileSize>Ord(Kilo) then
+    Result := IntToStr(FileSize div Ord(Kilo))+','+
+              IntToStr((FileSize mod Ord(Kilo)))+'KiB'
+  else
+    Result := IntToStr(FileSize)+'B';
+end;
+
+function tFichier.GetOctetToString: string;
+begin
+  Result := IntToStr(FileSize)+'B';
+end;
+
+procedure tFichier.SetPath(const Value: string);
+begin
+  FPath := Value;
+end;
+
+function tFichier.GetDates(const Index: tKindAccess): TDateTime;
+begin
+  Result := fDates[Index];
+end;
+
+procedure tFichier.SetDates(const Index: tKindAccess; Value: TDateTime);
+begin
+  fDates[Index] := Value;;
+end;
+
+{ tCritere }
+
+function tCritere.AcceptFile(Fic: tFichier): Boolean;
+var Limite : Integer;
+var D,M,Y : Word;
+begin
+  case Period of
+    kpDay   : Limite := Value;
+    kpMonth : Limite := Value*31;
+    kpWeek  : Limite := Value*7;
+    kpYear  : Limite := Value*365;
+  end;
+  Result := DaysBetween(Date,Fic.Dates[Access])>=Limite;
+end;
+
+constructor tCritere.Create(StValue: string);
+begin
+  Value := -1;
+  Access := kaAccessed;             { Le plus restrictif }
+  fSpec := TStringList.Create;
+  StValue := Trim(StValue);
+  StValue := StringReplace(StValue,' ','',[rfReplaceAll]);
+  if StValue<>'' then
+  begin
+    if (StValue[1]='(') and
+      (Length(StValue)>2) and
+      (StValue[3]=')') Then
+    begin
+      case UpCase(StValue[2]) of
+        'A' : Access := kaAccessed;
+        'M' : Access := kaModified;
+        'C' : Access := kaCreated;
+        else raise tCleanException.Create('Mauvais code d''accès fichier :['+Upcase(StValue[2]));
+      end;
+      Delete(StValue,1,3);
+    end;
+    if (StValue[1]='+') and
+      (Length(StValue)>2) and
+      (Pos('[',StValue)<>0) and
+      TryStrToInt(Copy(StValue,2,Pred(Pred(Pos('[',StValue)))-1),fValue) Then
+    begin
+      case UpCase(StValue[Pred(Pos('[',StValue))]) of
+        'D' : Period := kpDay;
+        'W' : Period := kpWeek;
+        'M' : Period := kpMonth;
+        'Y' : Period := kpYear;
+        else raise tCleanException.Create('Mauvais code de période :['+UpCase(StValue[Pred(Pos('[',StValue))]));
+      end;
+      Delete(StValue,1,Pred(Pos('[',StValue)));
+    end;
+    if (StValue[1]='[') and
+      (Length(StValue)>2) and
+      (StValue[Length(StValue)]=']') Then
+    begin
+      Delete(StValue,1,1);
+      SetLength(StValue,Length(StValue)-1);
+      while Pos(';',StValue)<>0 do
+      begin
+        Spec.Add(Copy(stValue,1,Pred(Pos(';',StValue))));
+        Delete(StValue,1,Pos(';',StValue));
+      end;
+      if StValue<>'' then
+        Spec.Add(stValue);
+    end
+    else
+      raise tCleanException.Create('Mauvaise Syntaxe :['+StValue+']');
+  end;
+end;
+
+destructor tCritere.Destroy;
+begin
+  fSpec.Free;
+  inherited;
+end;
+
+procedure tCritere.SetAccess(const Value: tKindAccess);
+begin
+  FAccess := Value;
+end;
+
+procedure tCritere.SetPeriod(const Value: tKindPeriod);
+begin
+  FPeriod := Value;
+end;
+
+procedure tCritere.SetSpec(const Value: TStrings);
+begin
+  FSpec := Value;
+end;
+
+procedure tCritere.SetValue(const Value: Integer);
+begin
+  FValue := Value;
+end;
+
+function tCritere.ToString: string;
+begin
+  Result := IntToStr(Value)+' '+ PeriodLib[Period]+' par rapport à la date de '+AccessLib[Access];
+end;
+
+{ tTask }
+
+constructor tTask.Create(Value: string);
+begin
+  Create;
+  ExploiteCritere(Value);
+end;
+
+constructor tTask.Create;
+begin
+  inherited ;
+  fListCriteres := TObjectList.Create;
+end;
+
+destructor tTask.Destroy;
+begin
+  fListCriteres.Free;
+  inherited;
+end;
+
+procedure tTask.ExploiteCritere(Crit: string);
+var LocalCritere : string;
+begin
+  LocalCritere := Crit;
+{  TraceSt.Add('On va exploiter : ['+Crit+']'); }
+  while LocalCritere<>'' do
+  begin
+    ExploiteOneCritere(LocalCritere)
+  end;
+end;
+
+procedure tTask.ExploiteOneCritere(var Crit: string);
+var Critere : tCritere;
+var Indice : Integer;
+begin
+  Indice := Pos(']',Crit);
+  if Indice<>0 then
+  begin
+    Critere := tCritere.Create(Copy(Crit,1,Indice));
+    ListCriteres.Add(Critere);
+    Crit := Copy(Crit,Succ(Indice),Length(Crit));
+//    TraceSt.Add('Critère créé (reste ['+Crit+']) :'+
+//            AccessLib[Critere.Access]+
+//            ', Valeur('+IntToStr(Critere.Value)+'),'+
+//            PeriodLib[Critere.Period]+', Spec{'+Critere.Spec.CommaText+'}');
+  end
+  else
+    Crit:='';
+end;
+
+procedure tTask.ExploitOption(Opt: string);
+var O : tOptions;
+begin
+  Opt := ','+Opt+',';
+  fOptionsSet := [];
+  for O:= Low(tOptions) to High(TOptions) do
+  begin
+    if Pos(','+SetOptionsLib[O]+',',Opt)<>0 then
+      OptionsSet[O] := True;
+    if Pos(','+ClrOptionsLib[O]+',',Opt)<>0 then
+      OptionsSet[O] := False;
+  end;
+end;
+
+function tTask.getOptionSet: tOptionset;
+begin
+  Result := fOptionsSet;
+end;
+
+function tTask.GetOptionsSet(const Index: tOptions): Boolean;
+begin
+  Result := Index in fOptionsSet;
+end;
+
+function tTask.GetOptionToString: string;
+var O : tOptions;
+begin
+  Result := '';
+  for O:= Low(tOptions) to High(TOptions) do
+  begin
+    if O in fOptionsSet then
+      Result := Result + SetOptionsLib[O]+', '
+    else
+      Result := Result + ClrOptionsLib[O]+', ';
+  end;
+  if Result<>'' then
+    Setlength(Result, Length(Result)-2);
+end;
+
+procedure tTask.SetOptionSet(const Value: tOptionset);
+begin
+  fOptionsSet := Value;
+end;
+
+procedure tTask.SetOptionsSet(const Index: tOptions; const Value: Boolean);
+begin
+  if Value then
+    fOptionsSet := fOptionsSet + [Index]
+  else
+    fOptionsSet := fOptionsSet - [Index];
+  bOptionsFOund := bOptionsFOund + [Index];  
+end;
+
+procedure tTask.SetTraceFile(const value: string);
+begin
+  fTraceFile := Value;
+  bTraceFile := true;
+end;
+
+function tTask.GetValueTraceFile(const value : string) : string;
+begin
+  if bTraceFile then
+    Result := TraceFile
+  else
+    result := Value;   
+end;
+
+function tTask.GetValueOptionSet(const value : tOptionSet) : tOptionSet;
+var Indice: tOptions;
+var Pos   : Boolean;
+begin
+  Result := [];
+  for Indice := Low(tOptions) to HIgh(tOptions) do
+  begin
+    Pos := false;
+    if (Indice in bOptionsFOund) then
+      Pos := Indice in fOptionsSet
+    else   
+      Pos := Indice in Value;
+    if Pos then
+      Result := Result + [Indice];  
+  end;       
+end;
+
+
+end.
---
+++ cleanlogs.dpr
@@ -0,0 +1,353 @@
+{ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+Marc Chauffour Brink's
+Nettoyage des fichiers traces.
+
+Principe :
+
+On va passer au programme des spécifications de fichiers du genre
+*.log, *.trc, *.tmp avec des critères de vieillesse des fichiers.
++3d pour + de 3 jours
++3w pour + de 3 semaines
++3m pour + de 3 mois
++3y pour + de 3 ans
+
+en paramˆtre on a donc juste besoin de connaitre le nom de fichier
+contenant les paramètres.
+
+(c) pour tester la date de création
+(a) pour tester la date de dernier accès
+(m) pour tester la date de dernière modification
+
+[default]
+tracefile=\\ssiegebabs01\bamboo\logs
+criteres=+3d[*.log;*.trc] +3w[PY*.tmp]
+
+[\\ssiegebabs01\bamboo\logs]
+criteres=+3d[*.log;*.trc] +3w[PY*.tmp]
+
+[\\ssiegebabs10\bamboo\logs]
+criteres=+3d[*.log;*.trc] +3w[PY*.tmp]
+tracefile=\\ssiegebabs01\bamboo\logs
+option=recursif|plat
+
+‚volutions :
+Possibilit‚ de s‚lectionner les r‚pertoires avec masque.
+On pourrait ainsi faire le Menage dans les r‚pertoires temporaire
+de la maniŠre suivante :
+[\\ssiegebabs01\c$\*\temp]
+[\\ssiegebabs01\c$\documents and settings\*\temp]
+
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= }
+program cleanlogs;
+
+{$APPTYPE CONSOLE}
+{ DONE 5 -oMarc -cOptions : Tester la r‚cursivit‚ et la mettre en place. }
+{ DONE 5 -oMarc -cSection [override] Qui permet de forcer un mode simulation par exemple 
+                  en cours. Reste le problŠme des options partielle. Actuellement tout ou rien
+                  il faudrait savoir si on a ou pas l'option (0,1,2,3) sur deux bits }
+{ TODO 5 -oMarc -cAjout de TryRmSubDir }
+{ TODO 5 -oMarc -cAjout de [\\ssiegebabs01\bamboo*\temp] ou [c:\documents and settings\*\local settings\temp] }
+{ TODO 5 -oMarc -cAjout de makezip avec zipfile= }
+{ TODO 5 -oMarc -cPossibilit‚ de purger les zip dans le mˆme .ini avec des critŠres de (nombres ou de date) }
+
+uses
+  Classes,
+  Contnrs,
+  IniFiles,
+  SysUtils,
+  windows,
+  paszlib,
+  Crt,
+  BaseObjects in 'BaseObjects.pas';
+
+type
+  tParameters = (parmTrace, parmCritere, parmOption);
+  tDelay = (dDay, dWeek, DMonth, dYear);
+  tTypeDate = (dtAccess, dtCreate, dtModified);
+
+var
+  TraceF : string;
+  TraceSt : TStrings;
+
+const
+  ParametersName : array[tParameters] of string = ('tracefile','criteres','options');
+  DefaultSection  = ':default';
+  OverrideSection = ':override';
+
+procedure GiveHelp();
+begin
+  writeln('Usage: cleanlogs NomDeFichier');
+end;
+
+var
+  Inif : tIniFile;
+  Sections : TStringList;
+  MaListe : TObjectList;
+  MonFichier : tFichier;
+  Taille : Cardinal;
+  Indice,Idx,I : Integer;
+  default: tTask;
+  over: tTask;
+  workTask : tTask;
+  workCrit : tCritere;
+  OptionsFind: Integer;
+  Fichier : TextFile;
+  EtatTrt : string;
+  Forced : Byte;
+  lError : Integer;
+  NbFiles : integer;
+
+function LoadFiles(Path : string; Spec : string) : integer;
+var SRec: TSearchRec;
+var Hnd : LongWord;
+begin
+  Result := 0;
+  if workTask.OptionsSet[opRecursif] then
+    OptionsFind := faDirectory;
+  Hnd:=FindFirst(Path+'\*.*', OptionsFind, SRec);
+  if Hnd = 0 then
+  begin
+    repeat
+      if (SRec.Name[1]<>'.') and
+         ((Srec.FindData.dwFileAttributes and Ord(faDirectory))<>0) then
+        Result := Result + LoadFiles(Path+'\'+SRec.Name, Spec);
+    until FindNext(SRec) <> 0;
+    FindClose(Hnd);
+  end;
+  OptionsFind := faAnyFile and not (faVolumeID or faDirectory);
+  Hnd := FindFirst(Path+'\'+Spec, OptionsFind, SRec);
+  if Hnd = 0 then
+  begin
+    repeat
+      MonFichier := tFichier.Create(Srec);
+//      writeln('trouv‚:'+Srec.name);
+      if WorkCrit.AcceptFile(MonFichier) then
+      begin
+        MaListe.Add(MonFichier);
+        MonFichier.Path := Path;
+        Taille := Taille + MonFichier.FileSize;
+        Inc(Result);
+      end
+      else
+        MonFichier.Free;
+    until FindNext(SRec) <> 0;
+    FindClose(Hnd);
+  end;
+end;
+
+procedure LoadIniData(WTask : tTask; wSections : tStrings);
+var Indice : Integer;
+begin
+  Indice := 0;
+  while Indice<Sections.Count do
+  begin
+    if LowerCase(Sections.Names[Indice])=ParametersName[parmTrace] then
+      WTask.TraceFile := Sections.ValueFromIndex[Indice]
+    else if LowerCase(Sections.Names[Indice])=ParametersName[parmOption] then
+      WTask.ExploitOption(Sections.ValueFromIndex[Indice])
+    else if LowerCase(Sections.Names[Indice])=ParametersName[parmCritere] then
+      WTask.ExploiteCritere(Sections.ValueFromIndex[Indice]);
+    Inc(Indice);
+  end;
+end;
+
+procedure LogTrace;
+
+  procedure InnerLog;
+  var Ix : Integer;
+  var Ts: TDateTime;
+  begin
+    Ts := Now;
+    Ix := 0;
+    Writeln(Fichier,FormatDateTime('dd/mm/yyyy hh:nn:ss',Ts)+
+                    ' | =-=-=-=-=-=-=-=-=-=-= DEBUT DE TRACE =-=-=-=-=-=-=-=-=-=-=');
+    while Ix<TraceSt.Count do
+    begin
+      Writeln(Fichier,TraceSt[Ix]);
+      Inc(Ix);
+    end;
+    Writeln(Fichier,FormatDateTime('dd/mm/yyyy hh:nn:ss',Ts)+
+                    ' | =-=-=-=-=-=-=-=-=-=-=-= FIN DE TRACE =-=-=-=-=-=-=-=-=-=-=');
+    flush(Fichier);
+    Close(Fichier);
+  end;
+
+begin
+  if TraceSt.Count>0 then
+  try
+    Assign(Fichier,TraceF);
+    Append(Fichier);
+    InnerLog;
+  except
+    Assign(Fichier,TraceF);
+    Rewrite(Fichier);
+    InnerLog;
+  end;
+  TraceSt.Clear;
+end;
+
+function CheckForce : Boolean;
+var MyChar : Char;
+begin
+  if Forced=0 then
+  begin
+    WriteLn('sur le chemin : '+workTask.Path);
+    WriteLn('Òtes vous s–r de vouloir supprimer '+IntToStr(MaListe.Count)+
+            ' fichiers pour '+IntToStr(Taille div 1024)+' Kilo Octets ? [O/N] ');
+    read(MyChar);
+    if (UpCase(MyChar) IN ['O','Y']) then
+      Forced := 1
+    else
+      Forced := 2;
+  end;
+  Result := Forced=1;
+end;
+
+begin
+  TraceSt := TStringList.Create;
+  try
+    TextColor(Green);
+    write('cleanlogs - Nettoyage p‚riodique des fichiers traces. ');
+    TextColor(Yellow);
+    writeln('V1.01');
+    TextColor(Green);
+    writeln('            Marc Chauffour - Septembre 2006');
+    writeln;
+    Assign(Output,'');
+    Rewrite(Output);
+    if ParamCount<>1 then
+      GiveHelp()
+    else
+    begin
+      try
+        Sections := TStringList.Create();
+        try
+          Inif := TIniFile.Create(ParamStr(1));
+          
+          inif.ReadSectionValues(DefaultSection,Sections);
+          Default := tTask.Create;
+          LoadIniData(default,Sections);
+          
+          inif.ReadSectionValues(OverrideSection,Sections);
+          over := tTask.Create;
+          LoadIniData(over,Sections);
+          
+          inif.ReadSections(Sections);
+          Indice := 0;
+          while Indice<Sections.Count do
+          begin
+            if ((Sections[Indice][1])<>':') then
+            begin
+              workTask := tTask.Create;
+              default.Add(workTask);
+              workTask.Path := Sections[Indice];
+              workTask.TraceFile := default.TraceFile;
+              workTask.OptionSet := default.OptionSet;
+            end;
+            inc(Indice)
+          end;
+          Idx := 0;
+          while Idx<default.Count do
+          begin
+            workTask := tTask(default[Idx]);
+            inif.ReadSectionValues(workTask.Path,Sections);
+            LoadIniData(WorkTask,Sections);
+            WorkTask.TraceFile := Over.GetValueTraceFile(WorkTask.TraceFile);
+            WorkTask.OptionSet := Over.GetValueOptionSet(WorkTask.OptionSet);
+            inc(Idx);
+          end;
+          Inif.Free;
+        except on e: Exception do
+          TraceSt.Add('Exception: '+E.Message);
+        end;
+      finally
+      end;
+
+
+      MaListe := TObjectList.Create(True);
+      Indice := 0;
+      while Indice<default.Count do
+      begin
+        workTask := tTask(default[Indice]);
+        Taille := 0;
+        TraceF := workTask.TraceFile;
+        Idx := 0;
+        NbFiles := 0;
+        WriteLn('Chemin:['+WorkTask.Path+']');
+        while Idx<workTask.ListCriteres.Count do
+        begin
+          workCrit := tCritere(workTask.ListCriteres[Idx]);
+          I := 0;
+          TraceSt.Add(FormatDateTime('dd/mm/yyyy hh:nn:ss',Now)+
+                      ' | Path['+workTask.Path +
+                      '] - Options[' + workTask.OptionToString+
+                      '] - Extensions['+workCrit.Spec.CommaText+
+                      '] - CritŠres['+WorkCrit.toString+']');
+          while I<workCrit.Spec.Count do
+          begin
+            NbFiles := NbFiles + LoadFiles(workTask.Path, workCrit.Spec[I]);
+            Inc(I);
+          end;
+          Inc(Idx);
+          WriteLn('* Fichier(s)['+inttostr(NbFiles)+'] Ko['+IntToStr(Taille div 1024)+']');
+          WriteLn;
+        end;
+        TraceSt.Add(FormatDateTime('dd/mm/yyyy hh:nn:ss',Now)+
+                    ' | Fichiers[' + IntToStr(MaListe.Count)+
+                    '] - TailleOctets['+IntToStr(Taille)+
+                    '] - TailleKiloOctets['+IntToStr(Taille div 1024)+
+                    '] - TailleMegaOctets['+IntToStr(Taille div (1024*1024))+
+                    '] - TailleGigaOctets['+IntToStr(Taille div (1024*1024*1024))+']');
+
+        Idx := 0;
+        Forced := 0;
+        while Idx<MaListe.Count do
+        begin
+          MonFichier := tFichier(MaListe[Idx]);
+          if workTask.OptionsSet[opExecute] then
+            try
+              if workTask.OptionsSet[opForce] or CheckForce then
+              begin
+                if DeleteFile(pchar(MonFichier.Path+'\'+MonFichier.FileName)) then
+                  EtatTrt := 'SUPPRIME'
+                else
+                begin
+                  lError := GetLastError;
+                  EtatTrt := '!('+IntToStr(lError)+')-'+SysErrorMessage(lError)+'!';
+                end;
+              end
+              else
+              begin
+                EtatTrt := 'ABANDON UTILISATEUR';
+              end;
+            except on e: Exception do
+              EtatTrt := 'ERREUR:'+e.Message;
+            end
+          else
+            EtatTrt := 'SIMULE';
+          TraceSt.Add(FormatDateTime('dd/mm/yyyy hh:nn:ss',Now)+
+                      ' | NÝ['+IntToStr(MonFichier.ID)+
+                      '] - Emplacement['+MonFichier.Path+
+                      '] - Fichier['+MonFichier.FileName+
+                      '] - Cr‚ation['+FormatDateTime('dd/mm/yyyy hh:nn:ss',MonFichier.DateCreated)+
+                      '] - Modification['+FormatDateTime('dd/mm/yyyy hh:nn:ss',MonFichier.DateModified)+
+                      '] - DernierAccŠs['+FormatDateTime('dd/mm/yyyy hh:nn:ss',MonFichier.DateAccessed)+
+                      '] - TailleHR['+MonFichier.TailleToString+
+                      '] - KiloOctets['+MonFichier.KiloToString+
+                      '] - Action['+EtatTrt+']');
+          Inc(Idx);
+        end;
+        if TraceF<>'' then
+          LogTrace;
+        inc(Indice);
+        MaListe.Clear;
+      end;
+    end;
+  finally
+    TraceSt.Free;
+  end;
+{
+
+}
+end.
